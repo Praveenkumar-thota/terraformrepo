bea551655b8ab75c5022d18266cb72043220cecc
Praveenkumar-thota <praveen_thota@terraform.us-west1-b.c.my-new-project-239511.internal>
2019-08-05 12:11:35 +0000

    module instance

---
+++ modules/instance/main.tf
@@ -0,0 +1,41 @@
+#Launch a compute engine instance
+
+#Data sources
+/*data "google_compute_zones" "available" {
+}
+
+*/
+
+#create instances
+resource "google_compute_instance" "compute_instance" {
+  provider = google
+  count        = "${var.counts}"
+  name         = "${var.instance_name}-${count.index + 1 }"
+  #zone     = data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)]
+  zone         = "${var.zone}"
+  project      = "${var.project}"  
+  machine_type = "${var.machine_type}"
+  
+    boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+      size = "${var.disk_size}"
+      type = "${var.disk_type}"
+    }
+  }
+
+  network_interface {
+    
+    subnetwork         = "${var.subnetwork}"
+    subnetwork_project = "${var.project}"  
+
+    /*access_config {
+      # Include this section to give the VM an external ip address
+    } */
+
+    
+  }
+  // Apply the firewall rule to allow external IPs to access this instance, change to variable
+  tags = "${var.tags}"
+}
+
---
+++ modules/instance/variables.tf
@@ -0,0 +1,24 @@
+#variables for instance creation
+
+variable "zone" {
+  description = "machine type."
+  default     = "us-west1-b"
+}
+
+variable "instance_name" {}
+variable "counts" {}
+variable "machine_type" {}
+variable "project" {}
+variable "subnetwork" {}
+variable "disk_type" {
+  default = "pd-standard"
+}
+
+variable "disk_size" {}
+variable "disk_image" {}
+variable "tags" {
+type = "list"
+}
+
+
+
