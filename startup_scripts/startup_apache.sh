74ddcfb1c1df5b838886454a4ddac9e9da382ba3
Praveenkumar-thota <53466178+Praveenkumar-thota@users.noreply.github.com>
2020-09-23 19:00:06 +0530

    Update startup_apache.sh

--- startup_scripts/startup_apache.sh
+++ startup_scripts/startup_apache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting"
-sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><title>Praveen</title><body><h1>Welcome to  Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html
+sudo apt-get update && sudo apt-get install apache2 -y
